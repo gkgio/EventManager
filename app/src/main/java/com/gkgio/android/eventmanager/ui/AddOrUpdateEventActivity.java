26731791be6f387d3a366a855a00f9cc22ca74d0
Georgy <gio.0000@mail.ru>
2017-06-12 03:58:26 +0300

    Промежуточные коммит - работа над активити добавления и редактироваиня записи

--- app/src/main/java/com/gkgio/android/eventmanager/ui/AddOrUpdateEventActivity.java
+++ app/src/main/java/com/gkgio/android/eventmanager/ui/AddOrUpdateEventActivity.java
@@ -1,6 +1,12 @@
 package com.gkgio.android.eventmanager.ui;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.gkgio.android.eventmanager.R;
+import com.gkgio.android.eventmanager.model.Event;
 
 /**
  * Created by Георгий on 11.06.2017.
@@ -8,4 +14,30 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class AddOrUpdateEventActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private Event event;
+    private int position;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        event = (Event) getIntent().getSerializableExtra(MainActivity.INTENT_EVENT_PARAM);
+        position = getIntent().getIntExtra(MainActivity.INTENT_POSITION_PARAM);
+
+
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        if (event == null)
+            toolbar.setTitle(R.string.title_activity_new_event);
+        else toolbar.setTitle(R.string.title_activity_update_event);
+    }
+
 }
